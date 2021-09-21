6c6aa4e294bff5fdd3019a22934aba11b6d22286
jaserud <udjaser@gmail.com>
2021-09-21 14:09:29 -0400

    throw RequeueableException from requeue function (#47)
    * trhow RequeueableException from requeue function
    
    * fmt

--- src/test/java/org/icgc_argo/workflow_graph_lib/polyglot/PolyglotTest.java
+++ src/test/java/org/icgc_argo/workflow_graph_lib/polyglot/PolyglotTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.Map;
 import lombok.val;
 import org.icgc_argo.workflow_graph_lib.exceptions.CommittableException;
-import org.icgc_argo.workflow_graph_lib.exceptions.NotAcknowledgeableException;
+import org.icgc_argo.workflow_graph_lib.exceptions.RequeueableException;
 import org.icgc_argo.workflow_graph_lib.polyglot.exceptions.GraphFunctionException;
 import org.icgc_argo.workflow_graph_lib.polyglot.exceptions.GraphFunctionValueException;
 import org.junit.jupiter.api.Test;
@@ -86,11 +86,11 @@ public class PolyglotTest {
   @Test
   public void testBuiltInRequeueThrowsNotAcknowledgeableException() {
     assertThrows(
-        NotAcknowledgeableException.class,
+        RequeueableException.class,
         () -> runMainFunctionWithData(JS, "return requeue(\"Testing REQUEUE\")", Map.of()));
 
     assertThrows(
-        NotAcknowledgeableException.class,
+        RequeueableException.class,
         () -> runMainFunctionWithData(PYTHON, "return requeue(\"Testing REQUEUE\")", Map.of()));
   }
 }
