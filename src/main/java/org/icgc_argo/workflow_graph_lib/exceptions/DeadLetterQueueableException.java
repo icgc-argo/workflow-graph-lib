b4cb5260d5866d2218531f8bbcbad0bfdbe8f247
jaserud <udjaser@gmail.com>
2021-10-13 09:38:59 -0400

    extend exceptions to take a tx that can be accessed as optional

--- src/main/java/org/icgc_argo/workflow_graph_lib/exceptions/DeadLetterQueueableException.java
+++ src/main/java/org/icgc_argo/workflow_graph_lib/exceptions/DeadLetterQueueableException.java
@@ -1,5 +1,7 @@
 package org.icgc_argo.workflow_graph_lib.exceptions;
 
+import com.pivotal.rabbitmq.stream.Transaction;
+
 /** Exceptions that are involuntary and not retryable (ie. dynamic code errors) */
 public class DeadLetterQueueableException extends GraphException {
   public DeadLetterQueueableException() {
@@ -12,5 +14,13 @@ public class DeadLetterQueueableException extends GraphException {
 
   public DeadLetterQueueableException(Exception exception) {
     super(exception);
+  }
+
+  public DeadLetterQueueableException(String exception, Transaction<?> tx) {
+    super(exception, tx);
+  }
+
+  public DeadLetterQueueableException(Exception exception, Transaction<?> tx) {
+    super(exception, tx);
   }
 }
